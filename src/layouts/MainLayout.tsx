784308a627987042460e21713c073fd0832894df
seunggonhyeon <hyunsg5638@gmail.com>
2025-03-23 23:51:18 +0900

    design:Sidebar 일부 개선 및 학생 정보 컴포넌트 그리드 구현

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -16,10 +16,8 @@ export const MainWrapper = styled.div`
 `;
 
 export const SideBarArea = styled.div`
-  width: 19.5rem;
+  width: 312px; // Sidebar랑 좀 안맞음. fixed라서 그런듯
   height: 100vh;
-
-  border: 1px solid black;
 `;
 
 export const SideBar = styled.div`
@@ -42,6 +40,7 @@ export const SideBar = styled.div`
     font-size: 0.75rem;
     color: #5f5f5f;
     font-weight: bold;
+    margin-left: 0.25rem;
     margin-bottom: 0.75rem;
   }
 
@@ -129,12 +128,16 @@ export const MenuTab = styled.div<{ enabled: boolean }>`
   }
 `;
 
-export const MainBody = styled.main`
-  width: 79vw;
+export const MainArea = styled.main`
+  padding-top: 3rem;
+  /* width: 79vw; */
+  width: 75rem; //1200px
   height: 100vh;
   flex: 1;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+
+  /* border-left: black 1px solid; */
 `;
 
 type MainLayoutProps = {
@@ -148,10 +151,10 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
         <SideBar>
           <UserNameBox>
             <UserLastName>
-              <span>박</span>
+              <span>이</span>
             </UserLastName>
             <UserRole>
-              <span className="name">박기쓱</span>
+              <span className="name">이승스껄</span>
               <span> 선생님</span>
             </UserRole>
           </UserNameBox>
@@ -194,7 +197,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
           </AccountMenuBox>
         </SideBar>
       </SideBarArea>
-      <MainBody>{children}</MainBody>
+      <MainArea>{children}</MainArea>
     </MainWrapper>
   );
 };
--- src/pages/ScorePage.tsx
+++ src/pages/ScorePage.tsx
@@ -1,16 +1,184 @@
 import React from "react";
 import styled from "styled-components";
 
-const MainBodyWrapper = styled.div`
-  font-size: 5rem;
-  color: black;
+const StudentInfoBody = styled.div`
+  margin-left: 0.5rem;
+  margin-right: 3rem;
+  width: 71.5rem;
+  height: 16rem;
+
+  border-radius: 1rem;
+  filter: drop-shadow(0 0 2em #d3d3d3);
+
+  display: flex;
+  flex-direction: row;
+
+  background-color: #f9f9f9;
+
+  color: #636262;
 `;
+
+const PictureArea = styled.div`
+  padding: 2rem 2rem 1.5rem 2rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const PictureInput = styled.img`
+  width: 7.5rem;
+  height: 10rem;
+  border: 1px solid #b5b5b5;
+
+  background-color: #e9e8e8;
+
+  color: black;
+  font-size: 0.75rem;
+`;
+
+const ChangePictureButton = styled.button`
+  border-color: #c9c9c9;
+
+  width: 7.5rem;
+  height: 2rem;
+  background-color: #ffffff;
+  color: black;
+  font-weight: bold;
+`;
+
+const GridArea = styled.div`
+  padding: 2.25rem 0 1.5rem 0;
+  width: 31.5rem; //504px;
+  border: 1px solid black;
+
+  span.fixed {
+    font-size: 1.25rem;
+    color: black;
+  }
+
+  display: grid;
+  grid-template-columns: 144px 144px 120px;
+  grid-template-rows: 57px 83px; // 57px vs 60px;
+  gap: 16px 48px;
+
+  .item {
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+
+    color: #636262;
+    font-size: 0.75rem;
+    font-weight: bold;
+  }
+
+  .item:nth-child(1) {
+    justify-content: space-between; // 양 끝으로 배치
+    grid-row: 1/1;
+    grid-column: 1/1;
+  }
+  .item:nth-child(2) {
+    grid-row: 2/2;
+    grid-column: 1/1;
+  }
+  .item:nth-child(3) {
+    justify-content: space-between;
+    grid-row: 1/1;
+    grid-column: 2/2;
+  }
+  .item:nth-child(4) {
+    grid-row: 2/2;
+    grid-column: 2/2;
+  }
+  .item:nth-child(5) {
+    justify-content: space-between;
+    grid-row: 1/1;
+    grid-column: 3/3;
+  }
+  .item:nth-child(6) {
+    grid-row: 2/2;
+    grid-column: 3/3;
+  }
+`;
+
+// 원래 144px 짜리 input인데 padding-left:차이로 -12, border 감안 -4,
+const LongInput = styled.input`
+  border: 1px solid #7c7c7c;
+  border-radius: 0.5rem;
+
+  width: 8rem; //128px;
+  height: 2rem;
+  background-color: white;
+  padding: 0 0 0 0.75rem;
+  font-weight: bold;
+  color: black;
+
+  &::placeholder {
+    // 해당 input 태그의 placeholder 색상 바꾸기
+    color: #d3d3d3;
+  }
+`;
+
+// 원래 120px 짜리 input인데 LongInput과 마찬가지 이유로 -16
+const NormalInput = styled.input`
+  border: 1px solid #7c7c7c;
+  border-radius: 0.5rem;
+
+  width: 6.5rem; // 104px;
+  height: 2rem;
+  background-color: white;
+  padding: 0 0 0 0.75rem;
+  font-weight: bold;
+  color: black;
+
+  &::placeholder {
+    // 해당 input 태그의 placeholder 색상 바꾸기
+    color: #d3d3d3;
+  }
+`;
+
+const ScoreDatabaseBody = styled.div``;
+
+const shortInput = styled.input;
 
 const ScorePage = () => {
   return (
-    <div>
-      <MainBodyWrapper>Scorepage</MainBodyWrapper>
-    </div>
+    <>
+      <StudentInfoBody>
+        <PictureArea>
+          <PictureInput />
+          <ChangePictureButton>이미지 등록/변경</ChangePictureButton>
+        </PictureArea>
+        <GridArea>
+          <div className="item">
+            <span>이름</span>
+            <LongInput placeholder="학생 이름 입력"></LongInput>
+          </div>
+          <div className="item">
+            <span>학년, 반</span>
+            <span className="fixed">2학년 4반</span>
+          </div>
+          <div className="item">
+            <span>전화번호</span>
+            <LongInput placeholder="010-XXXX-XXXX"></LongInput>
+          </div>
+          <div className="item">
+            <span>생년월일</span>
+            <LongInput placeholder="YYYY-MM-DD"></LongInput>
+          </div>
+          <div className="item">
+            <span>총 성적</span>
+            <NormalInput placeholder="98"></NormalInput>
+          </div>
+          <div className="item">
+            <span>평균 등급</span>
+            <NormalInput placeholder="B+"></NormalInput>
+          </div>
+        </GridArea>
+      </StudentInfoBody>
+      <ScoreDatabaseBody />
+    </>
   );
 };
 
