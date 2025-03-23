dc720eee4737d014e2e46825a8f79adc66e90d03
seunggonhyeon <hyunsg5638@gmail.com>
2025-03-23 17:44:30 +0900

    design:sidebar 및 MainLayout  퍼블리싱

---
+++ src/pages/ScorePage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainBodyWrapper = styled.div`
+  font-size: 5rem;
+  color: black;
+`;
+
+const ScorePage = () => {
+  return (
+    <div>
+      <MainBodyWrapper>Scorepage</MainBodyWrapper>
+    </div>
+  );
+};
+
+export default ScorePage;
