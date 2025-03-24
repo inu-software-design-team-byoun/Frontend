11f0f25ca376dc529fdceafe3d4d18369ec0e4b5
seunggonhyeon <hyunsg5638@gmail.com>
2025-03-24 20:55:10 +0900

    design:학생정보컴포넌트 내  그리드 퍼블리싱 완료

--- src/pages/ScorePage.tsx
+++ src/pages/ScorePage.tsx
@@ -49,9 +49,9 @@ const ChangePictureButton = styled.button`
 `;
 
 const GridArea = styled.div`
+  /* border: 1px solid black; */
   padding: 2.25rem 0 1.5rem 0;
   width: 31.5rem; //504px;
-  border: 1px solid black;
 
   span.fixed {
     font-size: 1.25rem;
@@ -64,7 +64,7 @@ const GridArea = styled.div`
   gap: 16px 48px;
 
   .item {
-    border: 1px solid black;
+    /* border: 1px solid black; */
     display: flex;
     flex-direction: column;
 
@@ -81,6 +81,29 @@ const GridArea = styled.div`
   .item:nth-child(2) {
     grid-row: 2/2;
     grid-column: 1/1;
+
+    justify-content: space-between;
+
+    div {
+      display: flex;
+      justify-content: space-between;
+
+      width: 10.5rem;
+    }
+
+    button {
+      width: 5rem;
+      height: 1.75rem;
+
+      border: 1px solid #c9c9c9;
+      border-radius: 0.25rem;
+
+      background-color: white;
+
+      color: black;
+      font-weight: bold;
+      font-size: 0.75rem;
+    }
   }
   .item:nth-child(3) {
     justify-content: space-between;
@@ -90,6 +113,10 @@ const GridArea = styled.div`
   .item:nth-child(4) {
     grid-row: 2/2;
     grid-column: 2/2;
+
+    span {
+      margin-bottom: 0.25rem;
+    }
   }
   .item:nth-child(5) {
     justify-content: space-between;
@@ -99,6 +126,14 @@ const GridArea = styled.div`
   .item:nth-child(6) {
     grid-row: 2/2;
     grid-column: 3/3;
+    span {
+      margin-bottom: 0.25rem;
+    }
+  }
+  .item:nth-child(9) {
+    grid-row: 3/3;
+    grid-column: 3/3;
+    align-items: flex-end;
   }
 `;
 
@@ -118,6 +153,12 @@ const LongInput = styled.input`
     // 해당 input 태그의 placeholder 색상 바꾸기
     color: #d3d3d3;
   }
+
+  &:focus {
+    border-color: black;
+    border-width: 1.5px;
+    outline: none;
+  }
 `;
 
 // 원래 120px 짜리 input인데 LongInput과 마찬가지 이유로 -16
@@ -136,13 +177,47 @@ const NormalInput = styled.input`
     // 해당 input 태그의 placeholder 색상 바꾸기
     color: #d3d3d3;
   }
+
+  &:focus {
+    border-color: black;
+    border-width: 1.5px;
+    outline: none;
+  }
+`;
+
+const CrudButton = styled.button<{ bgColor: string; width?: string }>`
+  border: none;
+  border-radius: 0.5rem;
+
+  margin-left: 0.75rem;
+  width: ${(props) => (props.width ? props.width : "4rem")};
+  height: 2rem;
+
+  /* background-color: #86acff; */
+  background-color: ${(props) => props.bgColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-weight: bold;
+  color: white;
+`;
+
+const GapBlankBody = styled.div`
+  /* border: 1px solid black; */
+
+  width: 71.5rem;
+  margin: 1.25rem 0;
+
+  display: flex;
+  justify-content: flex-end;
 `;
 
 const ScoreDatabaseBody = styled.div``;
 
-const shortInput = styled.input;
+// const shortInput = styled.input;
 
-const ScorePage = () => {
+const ScorePage: React.FC = () => {
   return (
     <>
       <StudentInfoBody>
@@ -158,6 +233,10 @@ const ScorePage = () => {
           <div className="item">
             <span>학년, 반</span>
             <span className="fixed">2학년 4반</span>
+            <div>
+              <button>상담 내역</button>
+              <button>피드백</button>
+            </div>
           </div>
           <div className="item">
             <span>전화번호</span>
@@ -175,8 +254,19 @@ const ScorePage = () => {
             <span>평균 등급</span>
             <NormalInput placeholder="B+"></NormalInput>
           </div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item">
+            <CrudButton bgColor="#86acff;">수정</CrudButton>
+          </div>
         </GridArea>
       </StudentInfoBody>
+      <GapBlankBody>
+        <CrudButton bgColor="#70C776;" width="5rem">
+          학생 추가
+        </CrudButton>
+        <CrudButton bgColor="#FF6969;">삭제</CrudButton>
+      </GapBlankBody>
       <ScoreDatabaseBody />
     </>
   );
