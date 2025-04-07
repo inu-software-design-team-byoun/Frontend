137d34e507310c797a0a13275276348ae0958169
seunggonhyeon <hyunsg5638@gmail.com>
2025-04-07 20:20:24 +0900

    feat:연동코드 작성

--- src/hooks/useScoreApi.ts
+++ src/hooks/useScoreApi.ts
@@ -1,6 +1,7 @@
 // useScoreApi.ts
 import { useEffect, useState } from "react";
 import { dummyScores } from "../data/dummyScores"; // 실제 API 연결 전용 임시 데이터
+import { ENDPOINTS } from "../constants/api";
 
 interface ScoreItem {
   subject: string;
@@ -22,9 +23,10 @@ export const useScoreApi = (grade: number, classNum: number) => {
     const fetchScores = async () => {
       try {
         // 실제 API 사용 시 주석 해제
-        // const res = await fetch(`/scores/${grade}/class/${classNum}`);
+        // const res = await fetch(ENDPOINTS.scores(grade, classNum));
         // const json = await res.json();
 
+        // 실제 API 사용시 주석처리
         const json = dummyScores; // 임시 데이터로 대체
 
         // GradeTable과 GradeRow에서 사용 중인 형식으로 변환
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3012, // 원하는 포트 번호로 변경
+  },
+});
