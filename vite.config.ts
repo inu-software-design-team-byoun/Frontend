33a5a87f6d1d3f15bcaabca216d148a2b75ce9bf
yuripbong <kyri200350@gmail.com>
2025-04-22 01:38:43 +0900

    set: hiedu 도메인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,13 @@
 // })
 
 // vite.config.ts
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3012, // 원하는 포트 번호로 변경
+    allowedHosts: ["hiedu.site"],
   },
 });
